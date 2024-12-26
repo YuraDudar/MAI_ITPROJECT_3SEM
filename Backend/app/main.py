103d164c887e4934fae05fffff53ddf5f00f7b00
MeDGranD <90983291+MeDGranD@users.noreply.github.com>
2024-12-27 01:55:14 +0300

    Update main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import login, registration, cart, favorites, users, products, recommendation
 
 api = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api.include_router(login.router)
 api.include_router(registration.router)
